bb5fbd87d55f6655722895548b70b2022eb38d4a
Nagy Zsolt <ngyzsolt@yahoo.com>
2020-06-12 14:53:06 +0300

    Login Page Added

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-echo "Hello World!!!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf.8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="robots" content="follow" />
+
+        <title>Login/Register Page</title>
+
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <bod>
+
+    <div class="background">
+        <div class="formBox">
+            <form class="loginInputGroup">
+                <input type="text" class="inputField" placeholder="Email" required="required" type="email" />
+                <input type="text" class="inputField" placeholder="Password" required="required" type="password" />
+                <button type="submit" class="submitBtn">Log in</button>
+
+            </from>
+
+            
+        </div>
+    </div>
+
+        <!-- UIkit CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/css/uikit.min.css" /> 
+
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit-icons.min.js"></script> 
+    </bod>
+</html>
